6846662bff88e28605429f7353ea7047be9210e6
Yossi Edri <4yossiedri@gmail.com>
2012-05-30 09:39:36 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AntricodeApp::Application.config.secret_token = 'aec866765a26beddded79f69d4139d809e6fab875d5f0a9a7dcf7e5519094b55b9b8e8ee51bc2bef7c5557129764a91c63355f190783b55420064b3d4417cd9f'
